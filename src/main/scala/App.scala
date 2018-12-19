ffe66bcb094dbac0cde0a059eeb79eaea35da6bc
Alper Simsek <alpersim94@gmail.com>
2018-12-19 17:49:45 +0300

    some refactoring

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -2,7 +2,9 @@ import org.apache.flink.api.scala._
 import org.apache.flink.util.Collector
 
 case class Data(date: String, productId: String, eventName: String, userId: String)
+
 case class ProductCount(productId: String, count: Int)
+
 object App {
 
   def main(args: Array[String]): Unit = {
@@ -12,57 +14,82 @@ object App {
 
     val eventsCSV = env.readTextFile(inputSourceDirectorty)
 
-    val dataset = eventsCSV.map{
+    val dataset = eventsCSV.map {
       row =>
         val splittedRow = row.split('|')
-        Data(splittedRow(0),splittedRow(1),splittedRow(2),splittedRow(3))
+        Data(splittedRow(0), splittedRow(1), splittedRow(2), splittedRow(3))
     }
 
     val eventFilter = dataset.filter(data => data.eventName == "view")
     val groupedView = uniqueViewCounts(dataset) // eventFilter
     val uniqueEvents = uniqueEventCounts(dataset)
-    val alleventOfUser47 = uniqueEventCounts(dataset.filter(_.userId=="47"))
+    val allEventsOfUser47 = uniqueEventCounts(dataset.filter(_.userId == "47"))
 
-    val user47 = dataset.filter(_.userId=="47").map{
+    val userAndEvents = dataset.map {
+      data =>
+        (data.userId, data.eventName)
+    }.distinct
+    val userCounts = userEventCounts(userAndEvents).filter(d => d._2 > 3)
+
+    val user47 = dataset.filter(_.userId == "47").map {
       data =>
         data.productId
     }.distinct()
+
     groupedView.print()
     println("........")
     uniqueEvents.print()
     println("........")
-    alleventOfUser47.print()
+    allEventsOfUser47.print()
     println("........")
-    dataset.filter(_.userId=="47").print()
+    dataset.filter(_.userId == "47").print()
     println("........")
     user47.print()
+    println("........")
+    userCounts.print()
+    println("........")
+    userAndEvents.filter(data => data._1 == "4").print()
   }
 
   def uniqueViewCounts(dataset: DataSet[Data]): DataSet[ProductCount] = {
-    dataset.groupBy(_.productId).reduceGroup{
-      (a, b: Collector[ProductCount]) =>
+    dataset.groupBy(_.productId).reduceGroup {
+      (subDataset, b: Collector[ProductCount]) =>
         var key: String = null
         var count = 0
 
-        for (d <- a) {
-          key = d.productId
-          count+=1
+        for (data <- subDataset) {
+          key = data.productId
+          count += 1
         }
         b.collect(ProductCount(key, count))
     }
   }
 
   def uniqueEventCounts(dataset: DataSet[Data]): DataSet[(String, Int)] = {
-    dataset.groupBy(_.eventName).reduceGroup{
-      (a, b: Collector[(String, Int)]) =>
+    dataset.groupBy(_.eventName).reduceGroup {
+      (subDataset, b: Collector[(String, Int)]) =>
         var key: String = null
         var count = 0
 
-        for (d <- a) {
-          key = d.eventName
-          count+=1
+        for (data <- subDataset) {
+          key = data.eventName
+          count += 1
         }
         b.collect((key, count))
     }
   }
+
+  def userEventCounts(dataset: DataSet[(String, String)]): DataSet[(String, Int)] = {
+    dataset.groupBy(_._1).reduceGroup {
+      (subDataset, b: Collector[(String, Int)]) =>
+        var key: String = null
+        var count = 0
+        for (data <- subDataset) {
+          key = data._1
+          count += 1
+        }
+        b.collect((key, count))
+    }
+  }
+
 }
